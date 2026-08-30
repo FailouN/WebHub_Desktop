533c488b042cb8e61695d87ff8d70755aa3f1f36
FailouN <bnryvghghvy@gmail.com>
2026-08-30 23:45:24 +0700

    Update build-blocker.js

--- build-blocker.js
+++ build-blocker.js
@@ -3,23 +3,13 @@ const fetch = require('cross-fetch');
 const fs = require('fs');
 
 async function build() {
-    console.log('Начинаю сборку бинарника (оптимизированная версия)...');
+    console.log('Начинаю сборку бинарника (оптимизированная версия для YouTube)...');
     
-    // Используем только стабильные и проверенные списки
+    // Используем стандартные списки, полностью совместимые с движком Ghostery
     const blocker = await ElectronBlocker.fromLists(fetch, [
-        // Базовые списки (реклама и трекеры)
         'https://easylist.to/easylist/easylist.txt',
         'https://easylist.to/easylist/easyprivacy.txt',
-        'https://easylist-downloads.adblockplus.org/ruadlist+easylist.txt',
-        
-        // uBlock Origin - Только необходимые фильтры (RAW ссылки!)
-        'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt',
-        'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt', // КРИТИЧНО: чинит сломанные сайты
-        'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt',
-        'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/quick-fixes.txt',
-        
-        // Удаляем раздражители (печенье и т.д.), но не трогаем ресурсы плееров
-        'https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances-cookies.txt'
+        'https://easylist-downloads.adblockplus.org/ruadlist+easylist.txt'
     ]);
 
     const buffer = blocker.serialize();
